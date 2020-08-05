c12c493e56fcf6234f5057f4afaa5e029770194d
zzho4662 <zzho4662@gmail.com>
2020-08-05 10:45:17 +0900

    login

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
+const users = require("./routes/users");
+
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/v1/users", users);
+
 const PORT = process.env.PORT || 6120;
 
 app.listen(PORT, () => {
-  console.log("App listening on port 5700!");
+  console.log("App listening on port 6120!");
 });
