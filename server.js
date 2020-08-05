8e1a09bb57df02ffac72d2b6a0af7bba4d00efc7
zzho4662 <zzho4662@gmail.com>
2020-08-05 13:00:12 +0900

    rental

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 const users = require("./routes/users");
+const book = require("./routes/book");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", users);
+app.use("/api/v1/book", book);
 
 const PORT = process.env.PORT || 6120;
 
